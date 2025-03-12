0b3e963e67efc4588b9c571f2b1bc9f991e177e2
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2025-03-12 12:40:03 +0100

    css

--- frontend/next/src/app/home/move/[slug]/page.jsx
+++ frontend/next/src/app/home/move/[slug]/page.jsx
@@ -3,7 +3,7 @@ const varPelicula = process.env.NEXT_PUBLIC_IMAGES;
 import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { peliculaSeleccionada,PeliculasOcupadasPelicula,CompraEntradas } from '../../../plugins/communicationManager';
+import { peliculaSeleccionada, PeliculasOcupadasPelicula, CompraEntradas } from '../../../plugins/communicationManager';
 
 export default function Page() {
     const filas = 12;
@@ -46,18 +46,19 @@ export default function Page() {
             idUser,
             pelicula: slug,
             butacas: butacasSeleccionadas.map(butaca => {
-                const [fila,columna]=butaca.split('-').map(Number); // Contruir el format de l'array per enviar
-                return {fila,columna}
+                const [fila, columna] = butaca.split('-').map(Number);
+                return { fila, columna };
             })
-        }; console.log(data);
+        };
+        console.log(data);
 
         try {
             const response = await CompraEntradas(data);
-            console.log(response)
+            console.log(response);
         } catch (error) {
             console.log(error);
         }
-    }
+    };
 
     const seleccionarButaca = (fila, columna) => {
         const butaca = `${fila}-${columna}`;
@@ -66,14 +67,13 @@ export default function Page() {
         } else {
             setButacasSeleccionadas([...butacasSeleccionadas, butaca]);
         }
-    };    
+    };
 
-    // Verificar si la butaca esta ocupada
     const estaOcupada = (fila, columna) => {
         return varButacasOcupadas.some(
             (butaca) => butaca.fila === fila && butaca.columna === columna
         );
-    };    
+    };
 
     useEffect(() => {
         if (slug) {
@@ -94,24 +94,19 @@ export default function Page() {
                         />
                     </div>
 
-                    <div id='infoPelicula' className="w-full md:w-1/2 p-6 flex flex-col justify-center">
-                        <h1 className="text-3xl font-bold text-blue-500 mb-4">
-                            {pelicula.nombre_pelicula}
-                        </h1>
-
+                    <div className="w-full md:w-1/2 p-6 flex flex-col justify-center">
+                        <h1 className="text-3xl font-bold text-blue-500 mb-4">{pelicula.nombre_pelicula}</h1>
                         <p className="text-gray-400 text-lg mb-2">
                             <span className="font-semibold text-blue-500">Categoria:</span> {pelicula.categoria}
                         </p>
-
                         <p className="text-gray-300 text-lg">
-                            <span className="font-semibold text-blue-500">Descripció:</span> {pelicula.descripcion}
+                            <span className="font-semibold text-blue-500">Descripción:</span> {pelicula.descripcion}
                         </p>
-
                         <button
                             onClick={mostrarSeleccionarButaca}
-                            className="bg-blue-500 text-white font-semibold py-2 px-6 rounded-lg hover:bg-blue-600 transition duration-300 mt-[30px]"
+                            className="bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold py-3 px-8 rounded-lg hover:scale-105 transition transform duration-300 shadow-lg mt-6"
                         >
-                            Seleccionar butaques
+                            Seleccionar butacas
                         </button>
                     </div>
                 </div>
@@ -120,19 +115,15 @@ export default function Page() {
             {mostrarButacas && (
                 <div className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center p-4 z-50">
                     <div className="bg-gray-800 rounded-lg p-6 w-full max-w-4xl overflow-y-auto">
-                        <button onClick={cerrarSala} className="bg-red-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-red-600 transition duration-300 mb-4">
-                            X
-                        </button>
-
+                        <div className='flex justify-between mb-4'>
+                            <button onClick={cerrarSala} className="text-red-500 font-semibold text-3xl hover:scale-110 transition transform duration-200">X</button>
+                            <button onClick={ferReserva} className='bg-green-500 text-white px-6 py-2 rounded-lg font-semibold hover:bg-green-600 transition duration-300'>Finalizar y comprar</button>
+                        </div>
                         <div className="text-center mb-6">
                             <Image src="/screen.svg" width={800} height={100} alt="Pantalla" className="mx-auto" />
-                            <p className='border border-white border-solid color-white-900 mt-[100px]' onClick={() => ferReserva()}>
-                                Finalitzar i comprar
-                            </p>
                         </div>
-
                         <div className="flex flex-col gap-2">
-                            {Array.from({ length: filas }).map((_, rowIndex) => (
+                        {Array.from({ length: filas }).map((_, rowIndex) => (
                                 <div key={rowIndex} className="flex gap-2 justify-center">
                                     {Array.from({ length: colum }).map((_, colIndex) => {
                                         const fila = rowIndex + 1;
@@ -142,16 +133,15 @@ export default function Page() {
                                         const esOcupada = estaOcupada(fila, columna);
 
                                         return (
-                                            <div key={columna} className={`cursor-pointer ${esOcupada ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={() => !esOcupada && seleccionarButaca(fila, columna)} >
+                                            <div key={columna} className={`cursor-pointer ${esOcupada ? 'opacity-50 cursor-not-allowed' : 'hover:scale-110 transition transform duration-200'}`} onClick={() => !esOcupada && seleccionarButaca(fila, columna)}>
                                                 <Image src="/seat.svg" width={40} height={40} alt="Butaca"
-                                                    style={{ filter: esOcupada ? 'invert(10%) sepia(10%) saturate(9000%)' : estaSeleccionada ? 'invert(70%) sepia(99%) saturate(9000%)' : 'none', }}
+                                                    style={{ filter: esOcupada ? 'grayscale(80%)' : estaSeleccionada ? 'invert(70%) sepia(99%) saturate(9000%)' : 'none', }}
                                                 />
                                             </div>
                                         );
                                     })}
                                 </div>
                             ))}
-
                         </div>
                     </div>
                 </div>
